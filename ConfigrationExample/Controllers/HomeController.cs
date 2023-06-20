8a754694d60c736dfa2e111df4b2166f8185c565
Şaban Dönmez <eren.donmez.11@gmail.com>
2023-06-21 02:02:17 +0300

    Proje dosyası ekle.

---
+++ ConfigrationExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ConfigrationExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ConfigrationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var v1 = _configuration["Ornek"];
+            var v2 = _configuration["Person:Name"];
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
